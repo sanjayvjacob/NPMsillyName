2300d2a960fe7d7b1e6a361281b9f9abba03e4bc
Sanjay V Jacob <76661259+sanjayvjacob@users.noreply.github.com>
2023-12-19 14:40:07 +0000

    Add Superheroname Generator

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // var generateName = require("sillyname");
-import generateName from "sillyName";
-var sillyName = generateName();
-
-console.log(`My name is ${sillyName}.`);
+// import generateName from "sillyName";
+// var sillyName = generateName();
+// console.log(`My name is ${sillyName}.`);
+import superheroes from "superheroes";
+const name = superheroes.random();
+console.log(`I am ${name}!`);
