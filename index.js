e5f987f3535dddf7897a9e525a7fb2a3930b0823
Sanjay V Jacob <76661259+sanjayvjacob@users.noreply.github.com>
2023-12-19 13:57:08 +0000

    Modify to ECMA script

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var generateName = require("sillyname");
+// var generateName = require("sillyname");
+import generateName from "sillyName";
 var sillyName = generateName();
 
 console.log(`My name is ${sillyName}.`);
